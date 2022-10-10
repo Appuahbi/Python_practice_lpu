b6c4f10a911cd3e9567d967a46f03c11e1d5e9d4
LAPTOP-EIFSPIHN\SUNIL <abhiram12appu@gmail.com>
2022-10-10 21:46:27 +0530

    Add file

--- Practice/ifElse.py
+++ Practice/ifElse.py
@@ -13,37 +13,6 @@ else:
 
 
 
-#take values of length and breadth from user
-#print if it is a square or not
-Length=int(input("Enter the length : "))
-Breadth=int(input("Enter the breadth : "))
-if Length==Breadth:
-    print("It is a square")
-
-else:
-    print("it is not a square")
-
-
-
-
-
-
-
-
-
-#A Company decided to give bonus of 1000Rs to
-#employe if his/her service is more than 5 years
-#Ask user their salary and year of service and print
-#the net bonus amount
-
-c=int(input("salary : "))
-d=int(input("How many years of service : "))
-e=1000
-if d > 5:
-    print("Eligible for 1000Rs bonus")
-    print(c+e)
-else:
-    print("Not eligible for bonus")
 
 
 
