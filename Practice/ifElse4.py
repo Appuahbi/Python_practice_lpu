86ba303836efd15001c943f0cace80c7e40c983a
LAPTOP-EIFSPIHN\SUNIL <abhiram12appu@gmail.com>
2022-10-10 21:58:54 +0530

    Adding file

---
+++ Practice/ifElse4.py
@@ -0,0 +1,11 @@
+
+#take values of length and breadth from user
+#print if it is a square or not
+Length=int(input("Enter the length : "))
+Breadth=int(input("Enter the breadth : "))
+if Length==Breadth:
+    print("It is a square")
+
+else:
+    print("it is not a square")
+    
