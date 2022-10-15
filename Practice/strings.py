ba69a0067dcde348de72bf1fe03fa8da78e140d6
LAPTOP-EIFSPIHN\SUNIL <abhiram12appu@gmail.com>
2022-10-15 16:50:01 +0530

    Adding

---
+++ Practice/strings.py
@@ -0,0 +1,10 @@
+a="love\nyou\nthree\nthousand"
+print(a.strip())
+# strip remove white spavces from beginning and the end
+
+print(a.replace("love","LOVE"))
+# replace,replace an item with another item
+b="Hello"
+c="World"
+d=b+" "+c
+print(d)
