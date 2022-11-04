d3ddcf2fcd08c2364315cefcdd56444f3ae15052
LAPTOP-EIFSPIHN\SUNIL <abhiram12appu@gmail.com>
2022-11-04 22:02:00 +0530

    Updating

---
+++ Practice/forLoop4.py
@@ -0,0 +1,18 @@
+#Acceept a number from the user
+#calculate and print the sum of all numbers
+#from 1 to input number
+#using for loop
+number=int(input())
+for i in range(1,number):
+    number=number+i
+
+print(number)
+
+
+
+
+userinput=int(input("Enter a number"))
+sum=0
+for j in range(1,userinput):
+    sum= sum+j
+    print(sum)
