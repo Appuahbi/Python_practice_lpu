bb3d631011966940ca291243ad7c8f5d450aaf2d
LAPTOP-EIFSPIHN\SUNIL <abhiram12appu@gmail.com>
2022-11-05 12:46:55 +0530

    ADD

---
+++ Practice/functionintro.py
@@ -0,0 +1,10 @@
+def add(num1,num2):
+    sum=num1+num2
+    print(sum)
+add(5,10)
+add(100,17)
+def substract(a,b):
+    difference=a-b
+    print(difference)
+substract(78,19)
+substract(78768,876)
